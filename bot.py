2954d872e1c9f61179fb5eb567a03979989bde3f
CyR1en <cyrien3519@gmail.com>
2020-04-20 03:21:47 -0600

    Extended commands.Bot

--- bot.py
+++ bot.py
@@ -1,37 +1,25 @@
 import logging
 import sys
+
+import discord
 from discord import Colour
 from discord.ext import commands
 
 from cog.poll import Poll
-from configuration import ConfigFile, ConfigNode
 
 logger = logging.getLogger(__name__)
 
 
-class Bot:
-    def __init__(self, color=Colour.from_rgb(15, 185, 177)):
+class Bot(commands.Bot):
+    def __init__(self, config_file, command_prefix="$", color=Colour.from_rgb(15, 185, 177), **options):
+        super().__init__(command_prefix, **options)
         self.color = color
-        self.config_file = ConfigFile("config")
+        self.config_file = config_file
         self.token = None
-        self.bot = commands.Bot(command_prefix=self.get_prefix())
-        self.bot.remove_command('help')
-        self.bot.add_cog(Poll(self.bot, self.config_file, self.color))
-        self.run()
+        self.remove_command('help')
+        self.add_cog(Poll(self, self.config_file, self.color))
 
-    def get_prefix(self):
-        prefix = self.config_file.get_string_node(ConfigNode.PREFIX)
-        return '$' if not prefix else prefix
-
-    def run(self):
-        self.token = self.config_file.get_string_node(ConfigNode.TOKEN)
-        if self.token == ConfigNode.TOKEN.get_value():
-            logger.warning("The config file is either newly generated or the token was left to its default value. \n"
-                           "Please enter your bot's token:")
-            try:
-                self.token = input()
-                self.config_file.set(ConfigNode.TOKEN, self.token)
-            except KeyboardInterrupt:
-                logger.error("Interrupted token input")
-                sys.exit()
-        self.bot.run(self.token)
+    async def on_ready(self):
+        game = "$poll"
+        await self.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=game))
+        logger.info('Successfully logged in as {}'.format(self.user))
